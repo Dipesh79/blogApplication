5e5a7b986b653274ca13d1bae69b61e560bf3b1b
Dipesh Khanal <dipeshkhanal79@gmail.com>
2024-09-26 19:45:09 +0545

    feat: base controller api setup

---
+++ app/Http/Controllers/ApiBaseController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
+
+class ApiBaseController extends Controller
+{
+    /**
+     * @param $data
+     * @param string $message
+     * @return JsonResponse
+     */
+    public function api_success($data, string $message): JsonResponse
+    {
+        return response()->json([
+            'status' => 200,
+            'data' => $data,
+            'message' => $message
+        ], 200);
+    }
+
+    /**
+     * @param Exception $data
+     * @return JsonResponse
+     */
+    public function api_error(Exception $data): JsonResponse
+    {
+        Log::error($data);
+        return response()->json([
+            'status' => $data->getCode(),
+            'data' => [],
+            'message' => $data->getMessage()
+        ], $data->getCode());
+    }
+
+    /**
+     * @param $data
+     * @param string $message
+     * @param int $code
+     * @return JsonResponse
+     */
+
+    public function custom_error($data, string $message, int $code): JsonResponse
+    {
+        return response()->json([
+            'status' => $code,
+            'errors' => $data,
+            'message' => $message
+        ], $code);
+    }
+}
