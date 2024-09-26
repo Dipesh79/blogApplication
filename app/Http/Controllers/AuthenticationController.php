0fa54b2c05489dd1087a1ee858b156a742744208
Dipesh Khanal <dipeshkhanal79@gmail.com>
2024-09-26 20:31:06 +0545

    feat: login api

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Resources\User\UserResource;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 
 class AuthenticationController extends ApiBaseController
@@ -28,6 +30,29 @@ class AuthenticationController extends ApiBaseController
             return $this->api_error($exception);
         }
         return $this->api_success($userResource, 'User created successfully');
+
+    }
+
+    /**
+     * @operationId Login
+     *
+     * @param LoginRequest $request
+     * @return JsonResponse
+     */
+    public function login(LoginRequest $request): JsonResponse
+    {
+        try {
+            $user = User::where('email', $request->email)->first();
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return $this->custom_error([], 'The provided credentials are incorrect.', '422');
+            }
+            $token = $user->createToken('auth_token')->plainTextToken;
+            $user->token = $token;
+            $userResource = new UserResource($user);
+        } catch (Exception $exception) {
+            return $this->api_error($exception);
+        }
+        return $this->api_success($userResource, 'User logged in successfully');
 
     }
 
--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::controller(AuthenticationController::class)->group(function () {
-    Route::post('/register','register')->name('register');
+    Route::post('/register', 'register')->name('register');
+    Route::post('/login', 'login')->name('login');
 });
